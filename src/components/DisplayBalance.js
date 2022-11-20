e3f92f8e2efebbf2be8425d707830b8e52581083
Natalja <natkja@gmail.com>
2022-11-20 22:55:08 +0200

    DisplayButton component

---
+++ src/components/DisplayBalance.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Statistic, StatisticLabel, StatisticValue } from "semantic-ui-react";
+
+function DisplayBalance({ title, value,  color="black", size="tiny"}) {
+
+    return (
+        <Statistic size='tiny'color={color}>
+        <StatisticLabel style={{textAlign:'left'}}>{title}</StatisticLabel>
+        <StatisticValue>{value}</StatisticValue>
+      </Statistic>
+    )
+}
+
+export default DisplayBalance
