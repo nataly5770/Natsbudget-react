d755040c7611839da36c5e7c195e3dd4c236c019
Natalja <natkja@gmail.com>
2022-11-21 22:02:32 +0200

    Added component called DisplayComponents to replace multiple displays

--- src/App.js
+++ src/App.js
@@ -1,28 +1,17 @@
 import { Container, Grid, GridColumn, GridRow, Icon, Segment } from 'semantic-ui-react';
 import './App.css';
 import DisplayBalance from './components/DisplayBalance';
+import DisplayBalances from './components/DisplayBalances';
 import MainHeader from './components/MainHeader';
 import NewEntryForm from './components/NewEntryForm';
 
 function App() {
   return (
     <Container>
-      <MainHeader title='Natsbudget'/>
-      < DisplayBalance title='Your balance' value='1253.54' size='small'/>
-     
-    <Segment textAlign='center'>
-      <Grid columns={2} divided>
-        <GridRow>
-          <GridColumn>
-          < DisplayBalance title='Income' value='1253.54' color='green'/>
-          </GridColumn>
+        <MainHeader title='Natsbudget'/>
+        < DisplayBalance title='Your balance' value='1253.54' size='small'/>
+      < DisplayBalances />
 
-          <GridColumn>
-          < DisplayBalance title='Expenses' value='623,50' color='pink'/>
-         </GridColumn>
-        </GridRow>
-      </Grid>
-      </Segment>
 
 <MainHeader title='History' type="h3"/>
      
---
+++ src/components/DisplayBalances.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Grid, GridColumn, GridRow, Segment } from 'semantic-ui-react'
+import DisplayBalance from './DisplayBalance'
+
+function DisplayBalances() {
+    return
+    <Segment textAlign='center'>
+    <Grid columns={2} divided>
+      <GridRow>
+        <GridColumn>
+        < DisplayBalance title='Income' value='1253.54' color='green'/>
+        </GridColumn>
+
+        <GridColumn>
+        < DisplayBalance title='Expenses' value='623,50' color='pink'/>
+       </GridColumn>
+      </GridRow>
+    </Grid>
+    </Segment>
+}
+
+export default DisplayBalances
